501281eb44f50526a0384aae3dc655e0991ea37f
Artur Felipe Moran <aobayojimbo@gmail.com>
2023-06-16 15:18:29 +0100

    Initial Commit

---
+++ ClientServerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClientServerApp
+//
+//  Created by Artur Felipe Moran on 16.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
