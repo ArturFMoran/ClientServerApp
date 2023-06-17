5e5ad7e68a146831662569523f5a1bfa8c36f5e7
ArturFMoran <aobayojimbo@gmail.com>
2023-06-17 18:44:41 +0100

    17.06

--- ClientServerApp/ViewController.swift
+++ ClientServerApp/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var welcomeView: UIView!
+    @IBOutlet weak var welcomLabel: UILabel!
+    @IBOutlet weak var loginAndPasswordStack: UIStackView!
+    @IBOutlet weak var loginTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func signIn(_ sender: Any)
+    {
+        (loginTextfield.text == "user") && (passwordTextfield.text == "password") ? print("ok") : print("something wrong")
+    }
+    
 }
 
